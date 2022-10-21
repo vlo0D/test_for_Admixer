bc9cbd7896b21dff0ebcb9e70288490d6c8a428f
Vlad Yurchenko <0vladislav1@gmail.com>
2022-10-21 16:18:26 +0300

    change field value

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Task1;
 
-var field = new Field(21, 0, 3); //Creating field 9x9 with numbers from 0 to 3
+var field = new Field(9, 0, 3); //Creating field 9x9 with numbers from 0 to 3
 
 field.Start();
